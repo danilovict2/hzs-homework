d0e89c18a64ede73301f27b293cd1d6a8fce7b26
danilovict2 <danilosindric@gmail.com>
2024-12-03 21:36:59 +0100

    Add login and register

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HelloController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_hello')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('hello/index.html.twig', [
-            'controller_name' => 'HelloController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
