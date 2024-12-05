30cbefb4b5bda2038015db174d744251d2985e18
majstorbob212 <stanticdejo@gmail.com>
2024-12-05 22:24:36 +0100

    added filters to rankings

---
+++ assets/vue/controllers/RankedColleges.vue
@@ -0,0 +1,110 @@
+<template>
+    <TheNavbar/>
+    <div class="filters">
+        <h4>Filteri</h4>
+        
+        <div class="country-filter">
+            <h5>Država</h5>
+            <label for="Srbija"><input type="checkbox" name="Srbija" id="serbia"> Srbija</label>
+            <label for="Hrvatska"><input type="checkbox" name="Hrvatska" id="croatia"> Hrvatska</label>
+            <label for="Slovenija"><input type="checkbox" name="Slovenija" id="Slovenia"> Slovenija</label>
+            <label for="Bosna"><input type="checkbox" name="Bosna" id="bosnia"> Bosna i Hercegovina</label>
+            <label for="Montenegro"><input type="checkbox" name="Montenegro" id="montenegro"> Crna Gora</label>
+            <label for="Makedonija"><input type="checkbox" name="Makedonija" id="macedonia"> Makedonija</label>
+        </div>
+    </div>
+    <div class="colleges-scroll">
+        <div class="college">
+            <div class="college-container">
+                <h1>Naziv Fakulteta</h1>
+                <h4>1# Na rang listi fakulteta</h4>
+                <div class="reviews-average">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <h4 style="display: inline-block; vertical-align: middle; margin-left: 10px;">4.0</h4>
+                </div>
+            </div>
+            <img src="/img/students.jpg" alt="" class="small-image">
+        </div>
+        <div class="college">
+            <div class="college-container">
+                <h1>Naziv Fakulteta</h1>
+                <h4>1# Na rang listi fakulteta</h4>
+                <div class="reviews-average">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <h4 style="display: inline-block; vertical-align: middle; margin-left: 10px;">4.0</h4>
+                </div>
+            </div>
+            <img src="/img/students.jpg" alt="" class="small-image">
+        </div>
+        <div class="college">
+            <div class="college-container">
+                <h1>Naziv Fakulteta</h1>
+                <h4>1# Na rang listi fakulteta</h4>
+                <div class="reviews-average">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <h4 style="display: inline-block; vertical-align: middle; margin-left: 10px;">4.0</h4>
+                </div>
+            </div>
+            <img src="/img/students.jpg" alt="" class="small-image">
+        </div>
+        <div class="college">
+            <div class="college-container">
+                <h1>Naziv Fakulteta</h1>
+                <h4>1# Na rang listi fakulteta</h4>
+                <div class="reviews-average">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <h4 style="display: inline-block; vertical-align: middle; margin-left: 10px;">4.0</h4>
+                </div>
+            </div>
+            <img src="/img/students.jpg" alt="" class="small-image">
+        </div>
+        <div class="college">
+            <div class="college-container">
+                <h1>Naziv Fakulteta</h1>
+                <h4>1# Na rang listi fakulteta</h4>
+                <div class="reviews-average">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <h4 style="display: inline-block; vertical-align: middle; margin-left: 10px;">4.0</h4>
+                </div>
+            </div>
+            <img src="/img/students.jpg" alt="" class="small-image">
+        </div>
+        <div class="college">
+            <div class="college-container">
+                <h1>Naziv Fakulteta</h1>
+                <h4>1# Na rang listi fakulteta</h4>
+                <div class="reviews-average">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <h4 style="display: inline-block; vertical-align: middle; margin-left: 10px;">4.0</h4>
+                </div>
+            </div>
+            <img src="/img/students.jpg" alt="" class="small-image">
+        </div>
+    </div>
+</template>
+
+<script setup></script>
--- assets/vue/controllers/TheNavbar.vue
+++ assets/vue/controllers/TheNavbar.vue
@@ -12,14 +12,14 @@
                 </div>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Početna stranica</a>
+                        <a class="nav-link" aria-current="page" href="/">Početna stranica</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/college-quiz">Profesionalna orijentacija</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Rang lista</a>
+                        <a class="nav-link" href="/rankings">Rang lista</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
