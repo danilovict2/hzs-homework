fb80167a78dcae1d60e44bd62f68a8491afff0fc
majstorbob212 <stanticdejo@gmail.com>
2024-12-05 21:36:58 +0100

    updated College page

--- assets/vue/controllers/TheNavbar.vue
+++ assets/vue/controllers/TheNavbar.vue
@@ -12,10 +12,10 @@
                 </div>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Početna stranica</a>
+                        <a class="nav-link active" aria-current="page" href="/">Početna stranica</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Profesionalna orijentacija</a>
+                        <a class="nav-link" href="/college-quiz">Profesionalna orijentacija</a>
                     </li>
 
                     <li class="nav-item">
