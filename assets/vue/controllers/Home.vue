410ea907d25daf489d261bf48b8153cc0f01228a
danilovict2 <danilosindric@gmail.com>
2024-12-05 20:08:16 +0100

    Extract Navbar to base.html.twig

--- assets/vue/controllers/Home.vue
+++ assets/vue/controllers/Home.vue
@@ -1,7 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <TheNavbar />
-
     <div class="first-section shadow-lg">
         <div class="students-image">
             <img src="/img/students.jpg" alt="students">
@@ -11,7 +8,8 @@
             <h2><b>KOJI TI NAJVIŠE ODGOVARA</b></h2>
             <hr>
             <p class="">
-                Pronalaženje pravog fakulteta za tebe ne treba biti teško. Zato na području bivše Jugoslavije, olakšavamo otkrivanje i povezivanje sa najboljim fakultetima za 
+                Pronalaženje pravog fakulteta za tebe ne treba biti teško. Zato na području bivše Jugoslavije,
+                olakšavamo otkrivanje i povezivanje sa najboljim fakultetima za
                 <a href="#">
                     <b>tebe</b>
                 </a>.
@@ -19,73 +17,32 @@
         </div>
     </div>
     <div class="between-section">
-        <h2>Tvoja pretraga je jedinstvena <br/> Baš kao i ti.</h2>
+        <h2>Tvoja pretraga je jedinstvena <br /> Baš kao i ti.</h2>
         <p>
-            Personalizujemo tvoju pretragu fakulteta prema tvojim interesovanjima <br/> kako bismo ti olakšali izbor u odabiru pravog fakulteta za tebe. 
-            <br/>
-            Proveri lokacije popularnih fakulteta na 
+            Personalizujemo tvoju pretragu fakulteta prema tvojim interesovanjima <br /> kako bismo ti olakšali
+            izbor u odabiru pravog fakulteta za tebe.
+            <br />
+            Proveri lokacije popularnih fakulteta na
             <a href="#map"><b>mapi</b>.</a>
         </p>
     </div>
-=======
-    <div class="first-section">
 
-        <TheNavbar />
-
-        <div class="first-section shadow-lg">
-
-            <div class="students-image">
-                <img src="/img/students.jpg" alt="students">
-            </div>
-            <div class="find-colledge md fs-6">
-                <h3>PRONAĐI FAKULTET</h3>
-                <h2><b>KOJI TI NAJVIŠE ODGOVARA</b></h2>
-                <hr>
-                <p class="">
-                    Pronalaženje pravog fakulteta za tebe ne treba biti teško. Zato na području bivše Jugoslavije,
-                    olakšavamo otkrivanje i povezivanje sa najboljim fakultetima za
-                    <a href="#">
-                        <b>tebe</b>
-                    </a>.
-                </p>
-            </div>
-        </div>
-
->>>>>>> refs/remotes/origin/frontend
-
-        <div class="between-section">
-            <h2>Tvoja pretraga je jedinstvena <br /> Baš kao i ti.</h2>
+    <div class="second-section shadow-lg fs-6">
+        <div class="find-colledge">
+            <h4 class="fw-light">ODABERI FAKULTET</h4>
+            <h2><b>SVE ŠTO TI JE POTREBNO DA ODLUČIŠ</b></h2>
+            <hr>
             <p>
-                Personalizujemo tvoju pretragu fakulteta prema tvojim interesovanjima <br /> kako bismo ti olakšali
-                izbor u odabiru pravog fakulteta za tebe.
-                <br />
-                Proveri lokacije popularnih fakulteta na
-                <a href="#map"><b>mapi</b>.</a>
+                Sa svim potrebnim informacijama o svim fakultetima na području bivše Jugoslavije, sa lakoćom ćeš
+                uporediti različite fakultete i odabrati onaj koji ti najviše odgovara.
             </p>
         </div>
-
-        <div class="second-section shadow-lg md fs-6">
-            <div class="find-colledge">
-                <h4 class="fw-light">ODABERI FAKULTET</h4>
-                <h2><b>SVE ŠTO TI JE POTREBNO DA ODLUČIŠ</b></h2>
-                <hr>
-                <p>
-                    Sa svim potrebnim informacijama o svim fakultetima na području bivše Jugoslavije, sa lakoćom ćeš
-                    uporediti različite fakultete i odabrati onaj koji ti najviše odgovara.
-                </p>
-            </div>
-            <div class="students-image">
-                <img src="/img/graduation.jpg" alt="students">
-            </div>
+        <div class="students-image">
+            <img src="/img/graduation.jpg" alt="students">
         </div>
-
-        <Map :markerCoordinates="markers"></Map>
     </div>
-<<<<<<< HEAD
-    <a id="map"></a>
     <Map :markerCoordinates="markers"></Map>
-=======
->>>>>>> refs/remotes/origin/frontend
+
 </template>
 
 <script setup>
